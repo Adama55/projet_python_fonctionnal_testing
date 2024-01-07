b23e4bb0304eb6c6274300f8be538eda92858dec
Adama55 <mafahfof55@gmail.com>
2024-01-07 20:02:42 +0100

    ajout de test avec parametrize

--- tests/test_add_restaurant_menu.py
+++ tests/test_add_restaurant_menu.py
@@ -1,20 +1,33 @@
 from sources.restaurant_menu import Menu 
+import pytest
 
-def test_add_menu_item():
+@pytest.mark.parametrize(
+    "restaurant_name, item_name, description, price, expected_result",
+    [
+        ("KFC", "hot dog", "tinder et sauce hot", 10.99, 
+         f"Menu item 'hot dog' added for KFC."),
+
+        ("Burger", "cheese", "steak et bun oval", 8.00, 
+         f"Menu item 'cheese' added for Burger."),
+
+        ("Boulanger", "Panini", "fromage rappé et poulet", 5.50, 
+         f"Menu item 'Panini' added for Boulanger."),
+
+        ("Malifood", "Mafé", "sauce rachide et du riz", 10.50, 
+         f"Menu item 'Mafé' added for Malifood.")
+        # Ajoutez ici d'autres jeux de données pour tester différents scénarios
+    ]
+)
+def test_add_menu_item(restaurant_name, item_name, description, price,expected_result):
     # Création d'une instance de la classe Menu
     menu_manager = Menu()
-
-    # Paramètres pour l'ajout d'un élément au menu
-    restaurant_name = "Mon Restaurant"
-    item_name = "Plat du jour"
-    description = "Délicieux plat principal"
-    price = 15.99
 
     # Appel de la méthode add_menu_item pour ajouter un élément au menu
     result = menu_manager.add_menu_item(restaurant_name, item_name, description, price)
 
     # Vérification si l'élément a été correctement ajouté
-    assert result == f"Menu item '{item_name}' added for {restaurant_name}."
+    assert result == expected_result 
+    #f"Menu item '{item_name}' added for {restaurant_name}."
 
     # Vérification si l'élément ajouté se trouve réellement dans le menu
     menu = menu_manager.get_menu(restaurant_name)
